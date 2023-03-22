fc8f2e9b1eaa6794040d04dded07e8f32ff1f0ff
dTap <imtapped@protonmail.com>
2023-03-22 15:16:41 +0700

    Added button count and onClick event

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -2,8 +2,18 @@ const ErrorExample = () => {
 
   let count = 0;
 
+  const handleClick = () => { 
+    count = count + 1;
+    console.log(count);
+  }
+
   return <div>
     <h2>{count}</h2>
+    <button type="button" onClick={handleClick}
+      className='btn'>
+      {/* className='btn' is coming from global styles */}
+      increase count
+    </button>
   </div>;
 };
 
